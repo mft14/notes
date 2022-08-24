18fa0b3f77857ee82f5d775a480c9d7d866b509b
mft14 <metalfort14@gmail.com>
2022-08-24 10:46:04 +0200

    Link Lister updated, html link still needs auto filename

--- python-scripts/html_link_lister.py
+++ python-scripts/html_link_lister.py
@@ -1,24 +1,29 @@
 import os
 
-input("Put in your path:")
-print()
-path = 'C:\\xampp\\htdocs\\mft14.github.io\\tools\\apps'
+print("HTML Link Lister by MFT14\n")
+
+yn = "" #Yes no chooser
+path = input("Please type in your path: ")
+path.replace("\\","/")
 isDirectory = os.path.isdir(path)
-targetblank = ""
 
 if(isDirectory is not True):
-    print("Your path is not a valid Direcotry. Check again")
+    print("Your path is not a valid Directory. Check again")
 
-else:
-    ask = input("Do you need target=blank? (Y/N) ")
-    if(ask == "y" or ask == "Y"):
+else: #add target blank into that placeholder string
+    yn = input("Do you need target=_blank? (Y/N) ")
+    if(yn.lower() == "y" or yn == "yes"):
         targetblank = " target=\"_blank\""
+    else: 
+        targetblank = ""
 
     list_files = os.listdir(path) #list all files in an array
     file = open("link_list.html", "w") #make new file
     for i in range(0,len(list_files)): #list all files using array length
-
         link_name = list_files[i][0:list_files[i].find('.')].title()#substring from 0 to dot, title makes uppercase
         file.write("<a href=\""+list_files[i]+"\""+targetblank+">"+link_name+"</a>\n")
-
     file.close
+
+    print("")
+    print("Successfully created!")
+    input("")
---
+++ python-scripts/test.py
@@ -0,0 +1,6 @@
+yn = input("Do you need target=blank? (Y/N) ")
+if(yn.lower() == "y" or yn == "yes"):
+    print("lower works")
+else: 
+    print("No")
+
