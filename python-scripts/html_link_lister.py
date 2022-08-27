9d7bc2ea7152e8e1c49394d85ad6e51ff0d8468d
Metal Fortress <111111364+mft14@users.noreply.github.com>
2022-08-27 14:04:11 +0200

    Update html_link_lister.py

--- python-scripts/html_link_lister.py
+++ python-scripts/html_link_lister.py
@@ -1,29 +1,38 @@
 import os
+from pathlib import Path  #core python module
 
-print("HTML Link Lister by MFT14\n")
+# input("Put in your path:")
 
-yn = "" #Yes no chooser
-path = input("Please type in your path: ")
-path.replace("\\","/")
+path = Path.cwd()
 isDirectory = os.path.isdir(path)
+targetblank = ""
+counter = 0
+
+print("path === " + str(path))
+input()
 
 if(isDirectory is not True):
-    print("Your path is not a valid Directory. Check again")
+    print("Your path is not a valid Direcotry. Check again")
 
-else: #add target blank into that placeholder string
-    yn = input("Do you need target=_blank? (Y/N) ")
-    if(yn.lower() == "y" or yn == "yes"):
+else:
+    ask = input("Do you need target=blank? (Y/N) ")
+    if(ask == "y" or ask == "Y"):
         targetblank = " target=\"_blank\""
-    else: 
-        targetblank = ""
 
     list_files = os.listdir(path) #list all files in an array
-    file = open("link_list.html", "w") #make new file
+    list_files = sorted(list_files)
+
+    print("list files === " + str(list_files)+"\n\n")
+    input()
+    file = open("_link_list.html", "w") #make new file
     for i in range(0,len(list_files)): #list all files using array length
+
         link_name = list_files[i][0:list_files[i].find('.')].title()#substring from 0 to dot, title makes uppercase
         file.write("<a href=\""+list_files[i]+"\""+targetblank+">"+link_name+"</a>\n")
+
+        print(list_files[i])
+        counter = counter + 1
+
     file.close
 
-    print("")
-    print("Successfully created!")
-    input("")
+print("Successfully created " + str(counter) + " link(s)! ")
